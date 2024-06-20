8c8fb35f168a61fcf7c77a34cc1f1e5f31d6390c
mohdshahzil <theshahzil@gmail.com>
2024-06-20 16:15:11 +0530

    api key corrected

--- src/redux/slices/newsSlice.ts
+++ src/redux/slices/newsSlice.ts
@@ -28,7 +28,7 @@ export const fetchNews = createAsyncThunk(
     searchTerm: string;
   }) => {
     const response = await fetch(
-      `https://gnews.io/api/v4/top-headlines?category=${category}&q=${searchTerm}&apikey=4c88f10d45f7203dadf621c5705bd177f&lang=en`
+      `https://gnews.io/api/v4/top-headlines?category=${category}&q=${searchTerm}&apikey=4c88f10d45f7203dadf621c5705bd177&lang=en`
     );
     return response.json();
   }
